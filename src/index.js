e2e54cb3a666ad3a5563487e70a040a26a2e0f15
Michael Weber <webermn15@gmail.com>
2019-04-06 15:58:35 -0500

    fixing redux update bug, hopefully lmao

--- src/index.js
+++ src/index.js
@@ -7,15 +7,22 @@ import { App } from './App';
 import kiwiApp from './reducers';
 import { getUserInfoFromToken } from './actions';
 
-const middleware = applyMiddleware(thunk)
+// const middleware = applyMiddleware(thunk)
 
-let store = createStore(
-	kiwiApp, 
-	compose(
-		middleware,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-)
+// let store = createStore(
+// 	kiwiApp, 
+// 	compose(
+// 		middleware,
+// 		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// 	)
+// )
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(kiwiApp,
+		composeEnhancers(
+			applyMiddleware(thunk),
+		)
+);
 
 const user = localStorage.getItem('kiwiTypeUser')
 
